49b01bc8b60de1512abcac9d2b4c3a9dd6883d13
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-11-01 16:36:36 -0500

    se esta modificando el registro de usuario

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,8 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 module.exports = (req, res, next) => {
-  const token = req.header("x-auth-token");
+  console.log(req.headers.authorization);
+  const token = req.headers.authorization;
 
   if (!token) {
     return res
@@ -14,11 +15,13 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
     User.findOne({ username: decoded.username }, (err, user) => {
       if (err || !user) {
         return res.status(401).json({ error: "Token no válido." });
       }
       req.user = user;
+      console.log(user.findOne);
       next();
     });
   } catch (ex) {
